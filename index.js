c410aa28c5b00c4a27dac403c954684271e90551
Felipe Pereira <fe.pereirajj@gmail.com>
2026-01-02 21:38:00 -0300

    Primeiro código em JavaScript

---
+++ index.js
@@ -0,0 +1,7 @@
+let nome = "Felipe";
+let idade = 27;
+let curso = "Análise e Desenvolvimento de Sistemas";
+
+console.log(`Olá, meu nome é ${nome}`);
+console.log(`Tenho ${idade} anos`);
+console.log(`Curso ${curso}`);
